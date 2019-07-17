6b87f12fe5bb1a6f93aad690253311e28ede3783
Michael <mikegulik@gmail.com>
2019-07-16 19:46:43 -0500

    Keep it going

--- 2048Assignment/Board.cpp
+++ 2048Assignment/Board.cpp
@@ -24,10 +24,16 @@ void Board::GenerateBoard()
 {
 	for (int a = 0; a < 4; a++)
 	{
+		Piece* tmp = a == 0 ? topLeftPiece : new Piece();
+
 		for (int b = 0; b < 3; b++)
 		{
-			Piece *tmp;
-			tmp = new Piece();
+			Piece* t = new Piece();
+			//take care of top pieces
+			if (a == 0) {
+				
+
+			}
 		}
 	}
 }
